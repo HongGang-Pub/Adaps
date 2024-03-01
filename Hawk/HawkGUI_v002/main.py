427e5f832585d3f7704c93a275ad211ecc488461
LHG <93388432+HongGang-Pub@users.noreply.github.com>
2024-03-01 14:58:37 +0800

    更新GUIv002版本文件

---
+++ Hawk/HawkGUI_v002/main.py
@@ -0,0 +1,150 @@
+# ///////////////////////////////////////////////////////////////
+#
+# BY: WANDERSON M.PIMENTA
+# PROJECT MADE WITH: Qt Designer and PySide6
+# V: 1.0.0
+#
+# This project can be used freely for all uses, as long as they maintain the
+# respective credits only in the Python scripts, any information in the visual
+# interface (GUI) can be modified without any implication.
+#
+# There are limitations on Qt licenses if you want to use your products
+# commercially, I recommend reading them on the official website:
+# https://doc.qt.io/qtforpython/licenses.html
+#
+# ///////////////////////////////////////////////////////////////
+
+# IMPORT PACKAGES AND MODULES
+# ///////////////////////////////////////////////////////////////
+from gui.uis.windows.main_window.functions_main_window import *
+import sys
+from functools import partial
+
+# IMPORT QT CORE
+# ///////////////////////////////////////////////////////////////
+from Hawk.HawkGUI_v002.gui.qt_core import *
+
+# IMPORT SETTINGS
+# ///////////////////////////////////////////////////////////////
+from gui.core.json_settings import Settings
+
+# IMPORT PY ONE DARK WINDOWS
+# ///////////////////////////////////////////////////////////////
+# MAIN WINDOW
+from gui.uis.windows.main_window import *
+
+# IMPORT PY ONE DARK WIDGETS
+# ///////////////////////////////////////////////////////////////
+
+# ADJUST QT FONT DPI FOR HIGHT SCALE AN 4K MONITOR
+# ///////////////////////////////////////////////////////////////
+# os.environ["QT_FONT_DPI"] = "96"
+
+# IF IS 4K MONITOR ENABLE 'os.environ["QT_SCALE_FACTOR"] = "2"'
+
+
+# MAIN WINDOW
+# ///////////////////////////////////////////////////////////////
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # SETUP MAIN WINDOw
+        # Load widgets from "gui\uis\main_window\ui_main.py"
+        # ///////////////////////////////////////////////////////////////
+        self.ui = UI_MainWindow()
+        self.ui.setup_ui(self)
+
+        # LOAD SETTINGS
+        # ///////////////////////////////////////////////////////////////
+        settings = Settings()
+        self.settings = settings.items
+        # SETUP MAIN WINDOW
+        # ///////////////////////////////////////////////////////////////
+        self.hide_grips = True  # Show/Hide resize grips
+        SetupMainWindow.setup_gui(self)
+
+        # Connect Function
+        # ///////////////////////////////////////////////////////////////
+        self.ui.load_pages.Sel_Config_file_Button.clicked.connect(partial(MainFunctions.Sel_Config_file_func, self))
+        self.ui.load_pages.Load_ROI_file_Button.clicked.connect(partial(MainFunctions.Load_ROI_file_func, self))
+
+    # LEFT MENU BTN IS CLICKED
+    # Run function when btn is clicked
+    # Check funtion by object name / btn_id
+    # ///////////////////////////////////////////////////////////////
+    def btn_clicked(self):
+        # GET BT CLICKED
+        btn = SetupMainWindow.setup_btns(self)
+
+        # LEFT MENU
+        # ///////////////////////////////////////////////////////////////
+
+        # HOME BTN
+        if btn.objectName() == "btn_home":
+            # Select Menu
+            self.ui.left_menu.select_only_one(btn.objectName())
+
+            # Load Page 1
+            MainFunctions.set_page(self, self.ui.load_pages.page_1)
+
+        # WIDGETS BTN
+        elif btn.objectName() == "btn_more_config":
+            # Select Menu
+            self.ui.left_menu.select_only_one(btn.objectName())
+
+            # Load Page 2
+            MainFunctions.set_page(self, self.ui.load_pages.page_2)
+
+        # LOAD USER PAGE
+        elif btn.objectName() == "btn_settings":
+            # Select Menu
+            self.ui.left_menu.select_only_one(btn.objectName())
+
+            # Load Page 3
+            MainFunctions.set_page(self, self.ui.load_pages.page_3)
+
+        print(f"Button {btn.objectName()}, clicked!")
+
+    # LEFT MENU BTN IS RELEASED
+    # Run function when btn is released
+    # Check funtion by object name / btn_id
+    # ///////////////////////////////////////////////////////////////
+    def btn_released(self):
+        # GET BT CLICKED
+        btn = SetupMainWindow.setup_btns(self)
+
+        # DEBUG
+        print(f"Button {btn.objectName()}, released!")
+
+    # RESIZE EVENT
+    # ///////////////////////////////////////////////////////////////
+    def resizeEvent(self, event):
+        SetupMainWindow.resize_grips(self)
+
+    # MOUSE CLICK EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        # SET DRAG POS WINDOW
+        p = event.globalPosition()
+        globalPos = p.toPoint()
+        self.dragPos = globalPos
+
+
+# SETTINGS WHEN TO START
+# Set the initial class and also additional parameters of the "QApplication" class
+# ///////////////////////////////////////////////////////////////
+if __name__ == "__main__":
+    # APPLICATION
+    # ///////////////////////////////////////////////////////////////
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("gui/icon.ico"))
+
+    # SHOW MAIN WINDOW
+    # ///////////////////////////////////////////////////////////////
+    window = MainWindow()
+    window.show()
+
+    # EXEC APP
+    # ///////////////////////////////////////////////////////////////
+    sys.exit(app.exec())
