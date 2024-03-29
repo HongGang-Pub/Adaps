ad2c7f8f711c0c88a73cea674158b1499569925b
LHG <93388432+HongGang-Pub@users.noreply.github.com>
2024-03-29 19:03:01 +0800

    更新HawkGUI V002

---
+++ Hawk/HawkGUI_v002/ROIMoreConfig.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QTableWidget
+from Hawk.HawkGUI_v002.gui.uis.pages.ui_ROI_OthersConfig import Ui_Dialog
+from PySide6.QtWidgets import *
+from Hawk.HawkGUI_v002.gui.widgets.py_table_widget import PyTableWidget
+from PySide6.QtCore import *
+from Hawk.HawkGUI_v002.gui.widgets import *
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()  # 这是类函数的名称
+        self.ui.setupUi(self)  # 运行类函数里的setupUi
+        self.styleFile = styleFile
+        with open(self.styleFile, 'r') as f:
+            qssStyle = f.read()
+        self.setStyleSheet(qssStyle)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    styleFile = r"./gui/themes/page_themes/light/lightstyle.qss"
+    win = MainWindow()
+
+    win.show()  # 显示窗口
+    sys.exit(app.exec())
