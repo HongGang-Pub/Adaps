3bfa075398f3191a8e4a39446d20cab8592ab33f
LHG <93388432+HongGang-Pub@users.noreply.github.com>
2025-03-11 11:40:39 +0800

    修改 MIPI model 的 BUG

--- AdapsChip/Hawk01/mipi_config_cal.py
+++ AdapsChip/Hawk01/mipi_config_cal.py
@@ -1,9 +1,12 @@
 # ////////////////////////////////////////////////
-# MIPI 满足时序要求时, 相关配置自动计算逻辑
+# MIPI 满足 DPHY 协议的时序要求时, DPHY 寄存器相关配置自动计算脚本
 # ////////////////////////////////////////////////
 def MIPI_CONFIG_Cal(SYS_CLK=330, MIPI_RATE=1500, TxEscClkDiv=16, TXHSByteClkDiv=8):
-        T_txesc_clk = 1000 / (SYS_CLK / (TxEscClkDiv+1))
-        T_txhsbyte_clk = 1000 / (MIPI_RATE / TXHSByteClkDiv)
+        F_txesc_clk = SYS_CLK / (TxEscClkDiv+1)
+        F_txhsbyte_clk = MIPI_RATE / TXHSByteClkDiv
+
+        T_txesc_clk = 1000 / F_txesc_clk
+        T_txhsbyte_clk = 1000 / F_txhsbyte_clk
         UI = 1000 / MIPI_RATE
 
         # 标准的时序要求持续时间
@@ -38,15 +41,15 @@ def MIPI_CONFIG_Cal(SYS_CLK=330, MIPI_RATE=1500, TxEscClkDiv=16, TXHSByteClkDiv=
         T_hs_trail = T_hs_trail_config * T_txhsbyte_clk
         T_all_aacu = T_lp_01 + T_hs_exit + T_hs_pre_zero + T_hs_trail
 
-        print(f"SYS_CLK: {SYS_CLK}M, MIPI_RATE: {MIPI_RATE}, T_txesc_clk: {T_txesc_clk:5.2f}ns, T_txhsbyte_clk: {T_txhsbyte_clk:5.2f}ns:")
-        print(f"\t   Item             : CONFIG |  T_cal |  T_std | T_default")
+        print(f"SYS_CLK: {SYS_CLK}MHz, MIPI_RATE: {MIPI_RATE} Gbps/Lane, F_txesc_clk: {F_txesc_clk:5.2f} MHz, F_txhsbyte_clk: {F_txhsbyte_clk:5.2f}MHz:")
+        print(f"\tItem                : CONFIG |  T_cal |  T_std | T_default")
         print(f"\tT_lpx               : {2:4}   | {T_lp_01:6.2f} | {T_std_lp_01:6.2f} | {T_default_lp_01:6.2f}")
         print(f"\tT_hs_exit    ('d43) : {T_hs_exit_config:4}   | {T_hs_exit:6.2f} | {T_std_hs_exit:6.2f} | {T_default_hs_exit:6.2f}")
         print(f"\tT_hs_prepare ('d44) : {T_hs_prepare_config:4}   | {T_hs_prepare:6.2f} | {T_std_hs_prepare:6.2f} | {T_default_hs_prepare:6.2f}")
         print(f"\tT_hs_pre_zero('d45) : {T_hs_zero_config:4}   | {T_hs_pre_zero:6.2f} | {T_std_hs_pre_zero:6.2f} | {T_default_hs_pre_zero:6.2f}")
         print(f"\tT_hs_trail   ('d46) : {T_hs_trail_config:4}   | {T_hs_trail:6.2f} | {T_std_hs_trail:6.2f} | {T_default_hs_trail:6.2f}")
         print(f"\tT_all_aacu          : {'':5}  | {T_all_aacu:6.2f} | {T_std_all_accu:6.2f} | {T_default_all_aacu:6.2f}")
-        print(f"\tTime saving @default:  {T_default_all_aacu-T_all_aacu:6.2f} ns")
+        print(f"\tTime saving @default:  {T_default_all_aacu-T_all_aacu:6.2f} ns\n")
 
 
 if __name__ == '__main__':
@@ -56,10 +59,8 @@ if __name__ == '__main__':
     SYS_CLK_Q = [330, 400]
     TxEscClkDiv_Q = [16, 20]
     MIPI_RATE_Q = [1200, 1500]
-    TXHSByteClkDiv = 8
     for clk_index in range(len(SYS_CLK_Q)):
         for mipi_index in range(len(MIPI_RATE_Q)):
             MIPI_CONFIG_Cal(SYS_CLK=SYS_CLK_Q[clk_index],
                             TxEscClkDiv=TxEscClkDiv_Q[clk_index],
-                            MIPI_RATE=MIPI_RATE_Q[mipi_index],
-                            TXHSByteClkDiv=TXHSByteClkDiv)
+                            MIPI_RATE=MIPI_RATE_Q[mipi_index])
